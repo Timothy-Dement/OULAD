a7f77ba72dd5b821a387a21a629e688c7586193f
Timothy-Dement <timothy.dement@gmail.com>
2019-03-21 17:36:33 -0400

    Update setup.sh to account for data.zip

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [ -f data.zip ]
-    then rm -rf data.zip
-fi
-
 if [ -d data ]
     then rm -rf data
 fi
 
-curl -o data.zip https://analyse.kmi.open.ac.uk/open_dataset/download
 unzip data.zip -d data
 
 python split.py

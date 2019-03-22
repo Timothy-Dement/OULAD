5883221791fb6448834ff596604b662ca7cfd176
Timothy-Dement <timothy.dement@gmail.com>
2019-03-22 18:48:37 -0400

    Adjust spacing

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echo '';
+
 if [ -d data ]
     then rm -rf data
 fi
 
 unzip data.zip -d data
 
-echo ''; echo '-- split.py --'; echo ''; echo ''; python split.py
-echo ''; echo '-- extract_dates.py --'; echo ''; echo ''; python extract_dates.py
-echo ''; echo '-- extract_activity.py --'; echo ''; echo ''; python extract_activity.py
-echo ''; echo '-- join.py --'; echo ''; echo ''; python join.py
+echo ''; echo '-- split.py --'; echo ''; python split.py
+echo ''; echo '-- extract_dates.py --'; echo ''; python extract_dates.py
+echo ''; echo '-- extract_activity.py --'; echo ''; python extract_activity.py
+echo ''; echo '-- join.py --'; echo ''; python join.py

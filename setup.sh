b918e088585b58929b256bd463089ee813da8c60
Timothy-Dement <timothy.dement@gmail.com>
2019-03-22 18:47:47 -0400

    Fix message in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ fi
 
 unzip data.zip -d data
 
-echo '\n-- split.py --\n\n'; python split.py
-echo '\n-- extract_dates.py --\n\n'; python extract_dates.py
-echo '\n-- extract_activity.py --\n\n'; python extract_activity.py
-echo '\n-- join.py --\n\n'; python join.py
+echo ''; echo '-- split.py --'; echo ''; echo ''; python split.py
+echo ''; echo '-- extract_dates.py --'; echo ''; echo ''; python extract_dates.py
+echo ''; echo '-- extract_activity.py --'; echo ''; echo ''; python extract_activity.py
+echo ''; echo '-- join.py --'; echo ''; echo ''; python join.py

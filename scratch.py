6ec01e76d35b9c857fb03b1fb3dcfd09992eba67
Timothy-Dement <timothy.dement@gmail.com>
2019-04-15 08:55:36 -0400

    ae-base results

--- scratch.py
+++ scratch.py
@@ -4,12 +4,9 @@ import pandas as pd
 import sys
 import time
 
-for clf in ['dt', 'knn', 'nb', 'rf', 'svm']:
+df = pd.read_csv('./data/vle.csv')
 
-    df = pd.read_csv(f'./results/{clf}-results.csv')
+types = df['activity_type'].sort_values().unique()
 
-    base_df = df[df['sample'] == 'non-smote']
-    smote_df = df[df['sample'] == 'smote']
-
-    base_df.to_csv(f'./results/{clf}_results.csv', index=False)
-    smote_df.to_csv(f'./results/{clf}_smote_results.csv', index=False)
+for t in types:
+    print(t)
